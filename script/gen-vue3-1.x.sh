8948ba0c5b7b8b7bb4b5302a3febcf974fc6e568
ganguo <ganguo>
2026-06-15 00:21:30 +0800

    changed something

--- script/gen-vue3-1.x.sh
+++ script/gen-vue3-1.x.sh
@@ -1,1 +1,7 @@
-script/vue3/1.x/gen-entry_form.sh
+#script/vue3/1.x/gen-entry_form@bnrlike.sh
+script/vue3/1.x/gen-entry_form@navypitch.sh
+#script/vue3/1.x/gen-paged_table.sh
+#script/vue3/1.x/gen-tabs_excel_form.sh
+#script/vue3/1.x/gen-display_form.sh
+#script/vue3/1.x/gen-official_form.sh
+#script/vue3/1.x/gen-fixed_table.sh
--- script/vue3/1.x/gen-official_form.sh
+++ script/vue3/1.x/gen-official_form.sh
@@ -2,12 +2,14 @@ export GUIDBASE_DATA_ROOT=/Users/christian/export/local/works/doublegsoft.io/gui
 export GUIDBASE_JAR=/Users/christian/export/opt/guidbase/protosys-plugin-guidbase-7.0-shaded.jar
 export PROJBASE_DATA_ROOT=/Users/christian/export/local/works/doublegsoft.io/projbase/03.Development/projbase-data
 export PROJBASE_JAR=/Users/christian/export/opt/projbase/protosys-plugin-projbase-7.0-shaded.jar
+export TATABASE_DATA_ROOT=/Users/christian/export/local/works/doublegsoft.io/tatabase/03.Development/tatabase-data
+export TATABASE_JAR=/Users/christian/export/opt/tatabase/protosys-plugin-tatabase-7.0-shaded.jar
 
 export OUTPUT_ROOT=out
 
-export SPEC=entry_form
-export APPNAME=entry_form
-export NAMESPACE=ef
+export SPEC=official_form
+export APPNAME=official_form
+export NAMESPACE=oof
 export MOBELBASE_MODEL=
 export GUIDBASE_MODEL=spec/$SPEC.guidbase
 export PROJECT_ROOT=$OUTPUT_ROOT/"$SPEC"-1.x
@@ -16,16 +18,16 @@ export PROJECT_ROOT=$OUTPUT_ROOT/"$SPEC"-1.x
 ##                                     VUE3                                   ##
 ##                                                                            ##
 ################################################################################
-REPOS=("javascript-desktop@vue3-1.x" \
-       "../html/html-desktop@bnrlike#vue3-1.x" "../tebot/tebot-universal@bnrlike@vue3-1.x")
+REPOS=("javascript-desktop@vue3-1.x" "javascript-sdk@vanilla-1.x" \
+       "../html/html-desktop@bnrlike#vue3-1.x")
 
 for repo in "${REPOS[@]}"
 do
 export TEMPLATE_ROOT=$GUIDBASE_DATA_ROOT/javascript/$repo
 
 java -jar $GUIDBASE_JAR \
---modelbase=$MOBELBASE_MODEL \
---guidbase=$GUIDBASE_MODEL \
+--modelbase-model=$MOBELBASE_MODEL \
+--guidbase-model=$GUIDBASE_MODEL \
 --template-root=$TEMPLATE_ROOT \
 --output-root=$PROJECT_ROOT \
 --license=LICENSE \
@@ -76,4 +78,40 @@ java -jar $PROJBASE_JAR \
 \[\]\
 \} 2>&1
 
-cd $PROJECT_ROOT && yarn install && yarn run build
+################################################################################
+##                                                                            ##
+##                                    TEST                                    ##
+##                                                                            ##
+################################################################################
+REPOS=("markdown/markdown-dataspec@llm-1.x" "tebot/tebot-bnrlike@selenium-1.x" \
+       "javascript/javascript-sdk@vanilla-1.x")
+
+for repo in "${REPOS[@]}"
+do
+export TEMPLATE_ROOT=$TATABASE_DATA_ROOT/$repo
+
+java -jar $TATABASE_JAR \
+--modelbase-model=$MOBELBASE_MODEL \
+--guidbase-model=$GUIDBASE_MODEL \
+--template-root=$TEMPLATE_ROOT \
+--output-root=$PROJECT_ROOT \
+--license=LICENSE \
+--globals=\
+\{\
+\"projectRoot\":\"$PROJECT_ROOT\",\
+\"application\":\"$APPNAME\",\
+\"namespace\":\"$NAMESPACE\",\
+\"artifact\":\"$APPNAME\",\
+\"version\":\"1.0.0\",\
+\"description\":\"\",\
+\"naming\":\"com.doublegsoft.jcommons.programming.javascript.JavaScriptConventions\",\
+\"globalNamingConvention\":\"com.doublegsoft.jcommons.programming.javascript.JavaScriptNamingConvention\",\
+\"language\":\"javascript\",\
+\"imports\":\
+\[\],\
+\"dependencies\":\
+\[\]\
+\} 2>&1
+done
+
+cd $PROJECT_ROOT && yarn install && yarn run build && cd ../..
